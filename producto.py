587087ea5c3a59ee5d90ab7a3f88421d865233c4
1545218 <138245806+1545218@users.noreply.github.com>
2023-07-13 12:43:52 -0500

    Add files via upload

---
+++ producto.py
@@ -0,0 +1,13 @@
+#crear una lista de productos 
+
+producto = ('marca':0 'nombre':0 'precio':0 'cantidad':0)
+
+#crear un diccionario
+
+lista_productos()
+while  True:
+    try:
+        NOMBREDELPRODUCTO
+
+
+
