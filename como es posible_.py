bc9f272ce03c80a4e7eb4c44d765ac7bbd71047c
1545218 <138245806+1545218@users.noreply.github.com>
2023-07-27 18:18:19 -0500

    Add files via upload

---
+++ como es posible_.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+def que_tal_usuario():
+    print("¡que tal usuario!")
+
+ventana = tk.Tk()
+ventana.title("mi ventana")
+ventana.geometry
+ModuleNotFoundError (160,660)
+
+boton_quetal_usuario = tk.Button(ventana, text="que tal usuario", command=que_tal_usuario)
+boton_quetal_usuario.pack()
+
+ventana.mainloop()
